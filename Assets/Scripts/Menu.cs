21049796326698e90fe0ea71bbc7805762250d9a
KELTON DE BRITO VAREJANO <46644365809@info.local>
2025-08-29 14:32:06 -0300

    feat: trocando de pc2

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class Menu : MonoBehaviour
@@ -5,16 +6,34 @@ public class Menu : MonoBehaviour
 
     public GameObject jogo;
 
+    public TextMeshProUGUI textoInicial;
+
+    bool iniciado;
 
     private void Update()
     {
-        //if (Input.GetTouch(0))
-        //{
+        bool isTouching = Input.touchCount > 0 &&
+                         Input.GetTouch(0).phase != TouchPhase.Ended &&
+                         Input.GetTouch(0).phase != TouchPhase.Canceled;
 
-        //}
-        
+        if (isTouching)
+        {
+            
+            iniciado = true;
+
+        }
+
+        if (!iniciado)
+        {
+            //LetterAnim();
+        }
     }
 
+    void LetterAnim()
+    {
+        float a = 0;
+        textoInicial.color = new Color(0f, 0f, 0f, 0.5f);
+    }
 
     void StartGame()
     {
